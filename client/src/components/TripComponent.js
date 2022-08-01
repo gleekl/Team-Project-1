8816b9a281c81bf1e7ae57e348420e8563c1f041
Gareth Lee <lkl.g@hotmail.com>
2022-08-01 20:09:32 +1000

    Commented out part of TripComponent that had errors.

--- client/src/components/TripComponent.js
+++ client/src/components/TripComponent.js
@@ -1,7 +1,14 @@
 const TripComponent = ({ trips }) => {
+  // const tripList = trips.map((trip) => {
+  //   return (
+      
+  //   )
+  // })
+
   return (
     <>
       <h1>Test TripComponent.</h1>
+      {/* {tripList} */}
     </>
   )
 }
