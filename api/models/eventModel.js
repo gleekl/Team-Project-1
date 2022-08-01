6e9a7aae147738f2ac08e19c7ea9c31699b5945b
Yinmu Liu <yinmu.liu@gmail.com>
2022-08-01 15:28:24 +1000

    setup event schema, refactor trip schema

---
+++ api/models/eventModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const eventSchema = new Schema({
+  title: { type: String, required: true },
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/dgb2gz29u/image/upload/v1657851570/empty-image_urwddn.jpg",
+  },
+  country: String,
+  city: String,
+  description: { type: String, required: true },
+  cost: Number,
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;
