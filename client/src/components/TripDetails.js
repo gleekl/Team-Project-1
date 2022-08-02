b3ee26d2d965506b6fde6c3a2a3db4805fa5b96f
Brendan Young <young.brendan6@gmail.com>
2022-08-02 16:40:43 +1000

    event cards complete

--- client/src/components/TripDetails.js
+++ client/src/components/TripDetails.js
@@ -1,11 +1,18 @@
 import { useParams } from "react-router-dom"
 import EventDetails from './EventDetails'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const TripDetails = ({ trips }) => {
   const { tripID } = useParams()
   const trip = trips.find((trip) => trip._id === tripID)
   console.log(trip);
-  const Events = trip.events.map((event) => {
+  const events = trip.events.map((event) => {
     return <EventDetails event={event} key={event._id}/>
   })
   return (
@@ -22,7 +29,7 @@ const TripDetails = ({ trips }) => {
         <span>End Date:</span> {trip.endDate}
       </h5>
       <h5>
-        <span>Events:</span> {Events}
+        <span>Events:</span> {events}
       </h5>
       <h5>
         <span>Total Cost:</span> ${trip.totalCost}
