fe7dbe195f579345043cfe303e466c06d7a676a4
Brendan Young <young.brendan6@gmail.com>
2022-08-02 17:30:58 +1000

    Trip details and event details card complete on trip show routes

--- client/src/components/TripDetails.js
+++ client/src/components/TripDetails.js
@@ -13,28 +13,77 @@ const TripDetails = ({ trips }) => {
   const trip = trips.find((trip) => trip._id === tripID)
   console.log(trip);
   const events = trip.events.map((event) => {
-    return <EventDetails event={event} key={event._id}/>
+    return <EventDetails event={event} key={event._id} />
   })
   return (
+
     <div className="trip-details">
-      <h3>Trip Details:</h3>
-      <h4>{trip.title}</h4>
-      <h5>
-        <span>Author:</span> {trip.author}
-      </h5>
-      <h5>
-        <span>Start Date:</span> {trip.startDate}
-      </h5>
-      <h5>
-        <span>End Date:</span> {trip.endDate}
-      </h5>
-      <h5>
-        <span>Events:</span> {events}
-      </h5>
-      <h5>
-        <span>Total Cost:</span> ${trip.totalCost}
-      </h5>
+      <Card sx={{ maxWidth: 480 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={trip.image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {trip.title}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {trip.author}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Trip Start Date: {trip.startDate}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Trip End Date: {trip.endDate}
+          </Typography>
+          <br />
+          <Typography gutterBottom variant="h7" component="div">
+            Total Cost: ${trip.totalCost}
+          </Typography>
+          <br />
+          <Typography gutterBottom variant="h7" component="div">
+            What did you get upto?
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {trip.description}
+          </Typography>
+          <CardActions>
+            <Button size="small">Edit</Button>
+            <Button size="small">Delete</Button>
+          </CardActions>
+          <br />
+          <Typography gutterBottom variant="h5" component="div">
+            Events {events}
+          </Typography>
+        </CardContent>
+        {/* <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+      </Card>
     </div>
+
+    // <div className="trip-details">
+    //   <h3>Trip Details:</h3>
+    //   <h4>{trip.title}</h4>
+    //   <h5>
+    //     <span>Author:</span> {trip.author}
+    //   </h5>
+    //   <h5>
+    //     <span>Start Date:</span> {trip.startDate}
+    //   </h5>
+    //   <h5>
+    //     <span>End Date:</span> {trip.endDate}
+    //   </h5>
+    //   <h5>
+    //     <span>Total Cost:</span> ${trip.totalCost}
+    //   </h5>
+    //   <h5>
+    //     <span>Events:</span> {events}
+    //   </h5>
+    // </div>
   )
 }
 
